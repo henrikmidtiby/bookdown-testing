7ff462cbbd6148855f0ca2c8b2ad4529a0bcb32c
Henrik Skov Midtiby <hemi@mmmi.sdu.dk>
2021-06-10 08:33:08 +0200

    Remember to delete all files prior to importing the newly built book.

--- _deploy.sh
+++ _deploy.sh
@@ -10,6 +10,7 @@ git config --global user.name "Henrik Skov Midtiby"
 
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/henrikmidtiby/bookdown-testing.git book-output
 cd book-output
+git rm -rf *
 cp -r ../_book/* ./
 git add --all *
 git commit -m"Update the book" || true
